03acfcb14d0fd723426a0ee0f0147b2fe1ac5ae1
Dobby <nag_bit_8@mail.ru>
2022-01-12 09:53:41 +0300

    1/12/22

---
+++ AT_QA_Training-Lesson_3/4_api_testing/conftest.py
@@ -0,0 +1,34 @@
+import pytest
+import requests
+
+
+class APIClient:
+    """
+    Упрощенный клиент для работы с API
+    Инициализируется базовым url на который пойдут запросы
+    """
+    def __init__(self, base_address):
+        self.base_address = base_address
+
+    def post(self, path="/", params=None, data=None, headers=None):
+        url = self.base_address + path
+        return requests.post(url=url, params=params, data=data, headers=headers)
+
+    def get(self, path="/", params=None):
+        return requests.get(url=self.base_address + path, params=params)
+
+
+# Тестовое API: https://jsonplaceholder.typicode.com
+def pytest_addoption(parser):
+    parser.addoption(
+        "--url",
+        action="store",
+        default="https://ya.ru",
+        help="This is request url"
+    )
+
+
+@pytest.fixture(scope="session")
+def api_client(request):
+    base_url = request.config.getoption("--url")
+    return APIClient(base_address=base_url)
