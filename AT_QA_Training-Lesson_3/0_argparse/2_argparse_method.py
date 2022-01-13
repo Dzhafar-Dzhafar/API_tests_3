1ead26174c8b4c63316f214974b5cdf3452268a2
Dobby <nag_bit_8@mail.ru>
2022-01-13 05:22:41 +0300

    1/12/22

--- AT_QA_Training-Lesson_3/0_argparse/2_argparse_method.py
+++ AT_QA_Training-Lesson_3/0_argparse/2_argparse_method.py
@@ -5,17 +5,17 @@ parser = argparse.ArgumentParser()
 """
     Все параметры для add_argument
     
-    name or flags - Either a name or a list of option strings, e.g. foo or -f, --foo.
-    action - The basic type of action to be taken when this argument is encountered at the command line.
-    nargs - The number of command-line arguments that should be consumed.
-    const - A constant value required by some action and nargs selections.
-    default - The value produced if the argument is absent from the command line.
-    type - The type to which the command-line argument should be converted.
-    choices - A container of the allowable values for the argument.
-    required - Whether or not the command-line option may be omitted (optionals only).
-    help - A brief description of what the argument does.
-    metavar - A name for the argument in usage messages.
-    dest - The name of the attribute to be added to the object returned by parse_args().
+    name or flags - либо имя, либо список строк параметров, например. foo или -f, --foo..
+    action - основной тип действия, которое необходимо предпринять, когда этот аргумент встречается в командной строке..
+    nargs - количество аргументов командной строки, которые следует использовать.
+    const - постоянное значение, необходимое для выбора некоторых действий и nargs.
+    default - Значение, создаваемое, если аргумент отсутствует в командной строке.
+    type - Тип, в который должен быть преобразован аргумент командной строки.
+    choices - контейнер допустимых значений для аргумента.
+    required - можно ли опустить параметр командной строки (только необязательные параметры).
+    help - Краткое описание того, что делает аргумент.
+    metavar - имя аргумента в сообщениях об использовании.
+    dest - имя атрибута, который будет добавлен к объекту, возвращаемому функцией parse_args().
 
 """
 
