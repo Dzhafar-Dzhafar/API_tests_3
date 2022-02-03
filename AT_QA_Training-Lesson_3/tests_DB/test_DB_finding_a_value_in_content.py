abb8542b7e932c9fc99816e02769f0b215186e96
Dobby <nag_bit_8@mail.ru>
2022-02-03 23:49:03 +0300

    2/4/2022

--- AT_QA_Training-Lesson_3/tests_DB/test_DB_finding_a_value_in_content.py
+++ AT_QA_Training-Lesson_3/tests_DB/test_DB_finding_a_value_in_content.py
@@ -20,7 +20,7 @@ def test_get_response_json_contents_0():
     }
 
 
-def test_get_response_json_contents_7():
+def test_get_response_json_contents_6():
     response_1 = requests.get("https://www.openbrewerydb.org/page-data/projects/page-data.json")
     response_body_1 = response_1.json()
     assert response_body_1['result']['data']["mdx"]["tableOfContents"]['items'][6] == {
