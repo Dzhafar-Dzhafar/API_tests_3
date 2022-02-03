2f885b34b396dcfef06b2a69f34c674b89d26a29
Dobby <nag_bit_8@mail.ru>
2022-02-03 23:47:55 +0300

    2/4/2022

---
+++ AT_QA_Training-Lesson_3/tests_DB/test_DB_finding_a_value_in_content.py
@@ -0,0 +1,29 @@
+import requests
+
+
+def test_get_response_json_contents_0():
+    response = requests.get("https://www.openbrewerydb.org/page-data/projects/page-data.json")
+    response_body = response.json()
+    assert response_body['result']['data']["mdx"]["tableOfContents"]['items'][0] == {
+        "url": "#flutter",
+        "title": "Flutter",
+        "items": [
+            {
+                "url": "#hopyard-built-with-flutter",
+                "title": "hopyard (built with Flutter)"
+            },
+            {
+                "url": "#open-brewery-db-flutter-package",
+                "title": "Open Brewery DB Flutter Package"
+            }
+        ]
+    }
+
+
+def test_get_response_json_contents_7():
+    response_1 = requests.get("https://www.openbrewerydb.org/page-data/projects/page-data.json")
+    response_body_1 = response_1.json()
+    assert response_body_1['result']['data']["mdx"]["tableOfContents"]['items'][6] == {
+        "url": "#add-project",
+        "title": "Add Project"
+    }
